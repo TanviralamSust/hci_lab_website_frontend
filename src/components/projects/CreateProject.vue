993a7df88a4aac191abbc42c043123bfbfe88933
gautamrealzero <gautam.zero@gmail.com>
2020-02-09 23:59:07 +0600

    create project added

---
+++ src/components/projects/CreateProject.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <form>
+      <v-text-field
+        v-model="project.title"
+        label="Title"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="project.description"
+        label="Description"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="project.duration"
+        label="Duration"
+        required
+      ></v-text-field>
+      <v-file-input 
+        v-model="project.projectImage"
+        accept="image/*" 
+        label="Image">
+      </v-file-input>
+      <v-btn class="mr-4" @click="submit">submit</v-btn>
+      <!-- <v-btn @click="clear">clear</v-btn> -->
+    </form>
+  </div>
+</template>
+<script>
+import Header from "../common/Header";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      project: {
+        title: "",
+        description: "",
+        duration: "",
+        projectImage: null
+      },
+    };
+  },
+  mounted() {
+    this.fetchProjects();
+  },
+  components: {
+    navbar: Header
+  },
+  methods: {
+      submit() {
+        let bodyFormData = new FormData();
+        bodyFormData.set('title', this.project.title);
+        bodyFormData.set('description', this.project.description);
+        bodyFormData.set('duration', this.project.duration);
+        bodyFormData.append('projectImage', this.project.projectImage)
+    
+        let config = {headers: {
+                'Content-Type': 'multipart/form-data', 
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6Imd6ZXJvIiwiaWF0IjoxNTgxMjY5ODc3LCJleHAiOjE1ODEzNTYyNzd9.UfBEfGrAimZRyHhRDgyZienNxojprHCrZr2YVeN6C98' 
+            }
+        };
+        axios.post( 'http://localhost:9001/projects',
+            bodyFormData,
+            config
+        )
+        .then(function (response) {
+            //handle success
+            console.log(response);
+        })
+        .catch(function (response) {
+            //handle error
+            console.log(response);
+        });
+      }
+  }
+};
+</script>
+<style>
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import Projects from '../components/projects/Projects';
+import CreateProject from '../components/projects/CreateProject';
 import Members from '../components/members/Members';
 import Notices from '../components/notices/Notices';
 import Home from '../components/home/Home';
@@ -11,6 +12,10 @@ export const routes = [
     {
         path : '/projects',
         component: Projects
+    },
+    {
+        path : '/projects/addProject',
+        component: CreateProject
     },
     {
         path : '/members',
